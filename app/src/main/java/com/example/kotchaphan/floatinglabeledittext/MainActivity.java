fab84066e4779443aee584604d4567974b48a36e
WeRockStar <vr_freestyle@outlook.com>
2015-09-13 03:41:30 +0700

    Add show errors

--- app/src/main/java/com/example/kotchaphan/floatinglabeledittext/MainActivity.java
+++ app/src/main/java/com/example/kotchaphan/floatinglabeledittext/MainActivity.java
@@ -3,11 +3,13 @@ package com.example.kotchaphan.floatinglabeledittext;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.design.widget.TextInputLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -17,6 +19,19 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        TextInputLayout inputUserLayout = (TextInputLayout) findViewById(R.id.inputUsernameLayout);
+        inputUserLayout.setErrorEnabled(true);
+        inputUserLayout.setError("maximum than 2 chars");
+
+        EditText editUsername = (EditText) findViewById(R.id.edtUsername);
+        editUsername.setError("Required");
+
+        EditText editPassword = (EditText) findViewById(R.id.edtPassword);
+        editPassword.setError("Required");
+
+        EditText editEmail = (EditText) findViewById(R.id.edtEmail);
+        editEmail.setError("Required");
 
     }
 
