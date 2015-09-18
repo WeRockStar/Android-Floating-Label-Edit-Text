3c6eeb90c3419659808cef535a3568f1e058164f
WeRockStar <vr_freestyle@outlook.com>
2015-09-18 23:41:57 +0700

    Add Regular Expression

--- app/src/main/java/com/example/kotchaphan/floatinglabeledittext/MainActivity.java
+++ app/src/main/java/com/example/kotchaphan/floatinglabeledittext/MainActivity.java
@@ -1,38 +1,122 @@
 package com.example.kotchaphan.floatinglabeledittext;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.CoordinatorLayout;
 import android.support.design.widget.Snackbar;
 import android.support.design.widget.TextInputLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class MainActivity extends AppCompatActivity {
+
+    private Toolbar toolbar;
+    private EditText editUsername;
+    private TextInputLayout inputUserLayout;
+    private EditText editPassword;
+    private TextInputLayout inputPasswordLayout;
+    private EditText editEmail;
+    private TextInputLayout inputEmailLayout;
+    private CoordinatorLayout rootLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+
+        initialView();
         setSupportActionBar(toolbar);
 
-        TextInputLayout inputUserLayout = (TextInputLayout) findViewById(R.id.inputUsernameLayout);
+
         inputUserLayout.setErrorEnabled(true);
-        inputUserLayout.setError("maximum than 2 chars");
+        inputUserLayout.setError("max than 2 chars");
 
-        EditText editUsername = (EditText) findViewById(R.id.edtUsername);
-        editUsername.setError("Required");
+        inputPasswordLayout.setErrorEnabled(true);
+        inputPasswordLayout.setError("max than 4 chars");
 
-        EditText editPassword = (EditText) findViewById(R.id.edtPassword);
-        editPassword.setError("Required");
+        inputEmailLayout.setErrorEnabled(true);
 
-        EditText editEmail = (EditText) findViewById(R.id.edtEmail);
-        editEmail.setError("Required");
+        editUsername.addTextChangedListener(new TextCheck(editUsername));
+        editPassword.addTextChangedListener(new TextCheck(editPassword));
+        editEmail.addTextChangedListener(new TextCheck(editEmail));
 
+    }
+
+    public void initialView() {
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+        inputUserLayout = (TextInputLayout) findViewById(R.id.inputUsernameLayout);
+        editUsername = (EditText) findViewById(R.id.edtUsername);
+        editPassword = (EditText) findViewById(R.id.edtPassword);
+        editEmail = (EditText) findViewById(R.id.edtEmail);
+        rootLayout = (CoordinatorLayout) findViewById(R.id.rootLayout);
+        inputPasswordLayout = (TextInputLayout) findViewById(R.id.inputPasswordLayout);
+        inputEmailLayout = (TextInputLayout) findViewById(R.id.inputEmailLayout);
+    }
+
+    public String validInput() {
+        String isValid = "";
+        String pattern = "^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\\\\.[A-Za-z]{2,6}$";
+        //pattern obj
+        Pattern regexPattern = Pattern.compile(pattern);
+        Matcher matcher = regexPattern.matcher(editEmail.getText().toString());
+
+        Log.i("Matcher", matcher.matches() + "");
+
+        if (editUsername.getText().toString().trim().equals("") || editUsername.getText().toString().trim().length() <= 2)
+            isValid += "user";
+        if (editPassword.getText().toString().trim().equals("") || editPassword.getText().toString().trim().length() <= 2)
+            isValid += " password";
+        if (editEmail.getText().toString().trim().equals("") || editEmail.getText().toString().trim().length() <= 2 || !matcher.matches())
+            isValid += " email";
+        return isValid;
+    }
+
+    class TextCheck implements TextWatcher {
+        private View view;
+
+        public TextCheck(View vView) {
+            this.view = vView;
+        }
+
+        @Override
+        public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+        }
+
+        @Override
+        public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+        }
+
+        @Override
+        public void afterTextChanged(Editable s) {
+            String str = validInput();
+            int id = view.getId();
+            switch (id) {
+                case R.id.edtUsername:
+                    if (str.contains("user"))
+                        editUsername.setError("Required");
+                    break;
+                case R.id.edtPassword:
+                    if (str.contains("password"))
+                        editPassword.setError("Required");
+                    break;
+                case R.id.edtEmail:
+                    if (str.contains("email"))
+                        editEmail.setError("Required or Invalid email");
+                    break;
+            }
+        }
     }
 
     @Override
